58a4f86e48ea393f876361a090ecbca751cfc7ed
lidongheng <huoyi-20@163.com>
2022-05-04 17:04:16 +0800

    完善promise方法

--- Promise.js
+++ Promise.js
@@ -10,13 +10,13 @@
     const RESOLVED = 'resolved'
     const REJECTED = 'rejected'
 
-    self.status = 'pending'
+    self.status = PENDING
     self.data = undefined
     self.callbacks= []
 
     function resolve (value) {
-      if (self.status !== 'pending') return
-      self.status = 'resolved'
+      if (self.status !== PENDING) return
+      self.status = RESOLVED
       self.data = value
       // 如果有待执行的callbacks，立即异步执行回调函数
       if (self.callbacks.length > 0) {
@@ -29,8 +29,8 @@
     }
 
     function reject (reason) {
-      if (self.status !== 'pending') return
-      self.status = 'rejected'
+      if (self.status !== PENDING) return
+      self.status = REJECTED
       self.data = reason
       if (self.callbacks.length > 0) {
         setTimeout(() => {
@@ -46,45 +46,114 @@
     } catch (error) {
       reject(error)
     }
-    
-
-    
-    /* 
-      Promise函数对象的resolve
-     */
-    Promise.resolve = function () {
-
-    }
-
-    Promise.reject = function () {
-
-    }
-    
+  
     /*
-     Promise的原型对象then
-     指定成功和失败的回调函数
-     返回一个新的Promise对象
+      Promise的原型对象then
+      指定成功和失败的回调函数
+      返回一个新的Promise对象
     */
     Promise.prototype.then = function (onResolved, onRejected) {
-        
+      onResolved = typeof onResolved === 'function' ? onResolved : value => value
+      onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason }
+      const self = this
+      return new Promise((resolve, reject) => {
+
+        function handle (callback) {
+          /*
+          1. 如果抛出异常，return的promise就会失败，reason就是error
+          2. 如果回调函数返回不是promise，return的promise就会成功，value是resolve函数的值
+          3. 如果回调函数返回是promise,return的promise就是这个promise的结果
+            */
+          try {
+            const result = callback(self.data)
+            if (result instanceof Promise) {
+              result.then(resolve, reject)
+            } else {
+              resolve(result)
+            }
+          } catch (error) {
+            reject(error)
+          }
+        }
+        if (self.status === PENDING) {
+          self.callbacks.push({
+            onResolved () {
+              handle(onResolved)
+            },
+            onRejected () {
+              handle(onRejected)
+            }
+          })
+        } else if (self.status === RESOLVED) {
+          setTimeout(() => {
+            handle(onResolved)
+          })
+        } else {
+          setTimeout(() => {
+            handle(onRejected)
+          })
+        }
+      })
     }
     /*
-     Promise的原型对象catch
-     指定失败的回调函数
-     返回一个新的Promise对象
+      Promise的原型对象catch
+      指定失败的回调函数
+      返回一个新的Promise对象
     */
     Promise.prototype.catch = function (onRejected) {
-        
+      return this.then(undefined, onRejected)
+    }
+}
+
+    /* 
+    Promise函数对象的resolve
+    */
+    Promise.resolve = function (value) {
+      return new Promise((resolve, reject) => {
+        if (value instanceof Promise) {
+          value.then(resolve, reject)
+        } else {
+          resolve(value)
+        }
+      })
+    }
+  
+    Promise.reject = function (reason) {
+      return new Promise((resolve, reject) => {
+        reject(reason)
+      })
     }
 
-    Promise.all = function () {
-        
-    }
-
-    Promise.race = function () {
-        
-    }
+  Promise.all = function (promises) {
+    let promiseCounter = 0    
+    return new Promise((resolve, reject) => {
+      const promiseArr = new Array(promises.length)
+      promises.forEach((p, index) => {
+        Promise.resolve(p).then(
+          value => {
+            promiseArr[index] = value
+            promiseCounter++
+            if (promiseCounter === promises.length) {
+              resolve(promiseArr)
+            }
+          },
+          reason => reject(reason))
+      })
+    })    
   }
+
+  Promise.race = function (promises) {
+    return new Promise((resolve, reject) => {
+      promises.forEach((p) => {
+        Promise.resolve(p).then(
+          value => resolve(value),
+          reason => reject(reason)
+        )
+      })
+    })
+  }
+  
 
   window.Promise = Promise
 })(window)
+
